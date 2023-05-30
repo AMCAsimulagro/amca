b93f996521968ad9e07591f750e0b64443aa6074
Amca <simulagro.amca@gmail.com>
2023-05-30 18:26:58 -0500

    Se agrego el icono

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,14 @@ import 'package:amca/ui/utils/amca_palette.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FlutterNativeSplash.remove();
   DependecyInjection.registerInjections();
   runApp(const MyApp());
 }
